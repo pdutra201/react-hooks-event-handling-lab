5536574a80ef87f8ecd0081707e68edce3c06906
philip dutra <monsterphilip10@gmail.com>
2023-05-28 14:42:26 -0400

    compelted lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+function handleChange(){
+    console.log("Entering password...")
+}
+function Keypad(){
     return (
-        <div></div>
+        
+        <input onChange={handleChange} type="password"/>
+        
     )
 }
 
-export default Keypad;
+export default Keypad
